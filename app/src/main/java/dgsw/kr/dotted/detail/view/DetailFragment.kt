3bb955416f741c807b11e95859cf6a0eabe01912
haesung7 <fildervy@dgsw.hs.kr>
2023-07-12 17:06:21 +0900

    Add: Detail Screen
    07/12

--- app/src/main/java/dgsw/kr/dotted/detail/view/DetailFragment.kt
+++ app/src/main/java/dgsw/kr/dotted/detail/view/DetailFragment.kt
@@ -4,35 +4,74 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import dgsw.kr.dotted.ARG_PARAM1
-import dgsw.kr.dotted.ARG_PARAM2
 import dgsw.kr.dotted.R
+import dgsw.kr.dotted.databinding.FragmentDetailBinding
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [DetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DetailFragment : Fragment() {
+
+class DetailFragment() : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var name: String? = null
+    private var address: String? = null
+    private var profileImage: Int = R.drawable.ic_company
+    private var major = null
+    private var pay = null
+    private var companyType: String? = null
+    private var employmentType = null
+    private var charge = null
+
+
+
+
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
+
+
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+
+        binding.companyName.setText(name)
+        binding.companyAddress.setText(address)
+        binding.companyPicture.setBackgroundResource(profileImage)
+        binding.companyPayType.setText(major)
+        binding.companyPay.setText(pay)
+        binding.companyScale.setText(companyType)
+        binding.companyRole.setText(employmentType)
+        binding.companyMaster.setText(charge)
+
+        binding.backTo.setOnClickListener {
+
+        }
+
+
+        return binding.root
+
+
+
+
+
     }
 
     companion object {
@@ -44,13 +83,11 @@ class DetailFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment DetailFragment.
          */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
--- app/src/main/java/dgsw/kr/dotted/home/view/MainActivity.kt
+++ app/src/main/java/dgsw/kr/dotted/home/view/MainActivity.kt
@@ -3,11 +3,18 @@ package dgsw.kr.dotted.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dgsw.kr.dotted.R
+import dgsw.kr.dotted.detail.view.DetailFragment
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, DetailFragment())
+            .commit()
+
     }
 }
