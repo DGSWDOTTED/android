6a9170ca8e45d38f145307f9d019e38c7e1cfe07
PARAOOO <nakim3159@gmail.com>
2023-07-12 15:06:08 +0900

    [UPDATE]

--- app/src/main/java/dgsw/kr/dotted/home/view/MainActivity.kt
+++ app/src/main/java/dgsw/kr/dotted/home/view/MainActivity.kt
@@ -2,7 +2,7 @@ package dgsw.kr.dotted.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dgsw.hs.dotted.R
+import dgsw.kr.dotted.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/dgsw/kr/dotted/map/data/MapCompanyData.kt
@@ -0,0 +1,10 @@
+package dgsw.kr.dotted.map.data
+
+data class MapCompanyData(
+
+
+    val com_name : String,
+    val address : String,
+    val employ : String
+
+)
